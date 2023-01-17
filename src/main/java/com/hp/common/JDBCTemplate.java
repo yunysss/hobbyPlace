aeb852da7988c47789baa24daa7863869a67f44d
suyeonleee <115603405+suyeonleee@users.noreply.github.com>
2023-01-17 23:58:10 +0900

    JDBCTemplate

--- src/main/java/com/hp/common/JDBCTemplate.java
+++ src/main/java/com/hp/common/JDBCTemplate.java
@@ -1,5 +1,105 @@
 package com.hp.common;
 
-public class JDBCTemplate {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 
+public class JDBCTemplate {
+	
+	public static void main(String[] args) {
+		getConnection();
+	}
+	
+	public static Connection getConnection() {
+		
+		Connection conn = null;
+		Properties prop = new Properties();
+		
+
+		String filePath = JDBCTemplate.class.getResource("/db/driver/driver.properties").getPath();
+
+		
+		try {
+			prop.load(new FileInputStream(filePath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			// jdbc driver 등록
+			Class.forName(prop.getProperty("driver")); // ojdbc6.jar 파일 WEB-INF/lib/ 안에 추가
+			// db의 url, 계정명, 비밀번호 제시해서 Connection 생성
+			conn = DriverManager.getConnection(prop.getProperty("url"), 
+											   prop.getProperty("username"),
+											   prop.getProperty("password"));
+			
+			conn.setAutoCommit(false);
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+		
+	}
+	
+	public static void commit(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.commit();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void rollback(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.rollback();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Connection conn) {
+		try {
+			if(conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(Statement stmt) {
+		try {
+			if(stmt != null && !stmt.isClosed()) {
+				stmt.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void close(ResultSet rset) {
+		try {
+			if(rset != null && !rset.isClosed()) {
+				rset.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
 }
