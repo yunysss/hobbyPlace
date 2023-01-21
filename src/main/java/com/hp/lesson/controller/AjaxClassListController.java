2c705857a748600f6ce22f8b590f191f34f84b33
yunysss <115604544+yunysss@users.noreply.github.com>
2023-01-22 07:33:25 +0900

    tuteemainpage 수정

--- src/main/java/com/hp/lesson/controller/AjaxClassListController.java
+++ src/main/java/com/hp/lesson/controller/AjaxClassListController.java
@@ -1,23 +1,29 @@
-package com.hp.common.controller;
+package com.hp.lesson.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.hp.lesson.model.service.LessonService;
+import com.hp.lesson.model.vo.Lesson;
+
 /**
- * Servlet implementation class TuteeMainPageController
+ * Servlet implementation class ClassAllListController
  */
-@WebServlet("/main.tee")
-public class TuteeMainPageController extends HttpServlet {
+@WebServlet("/listMain.cl")
+public class AjaxClassListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public TuteeMainPageController() {
+    public AjaxClassListController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,7 +32,11 @@ public class TuteeMainPageController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("views/common/tuteeMainPage.jsp").forward(request, response);
+		ArrayList<Lesson> list = new LessonService().selectMainClass();
+		
+		response.setContentType("application/json; charset=UTF-8");
+		new Gson().toJson(list, response.getWriter());
+		
 	}
 
 	/**
--- src/main/java/com/hp/lesson/model/service/LessonService.java
+++ src/main/java/com/hp/lesson/model/service/LessonService.java
@@ -1,5 +1,20 @@
 package com.hp.lesson.model.service;
 
+import static com.hp.common.JDBCTemplate.*;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import com.hp.lesson.model.dao.LessonDao;
+import com.hp.lesson.model.vo.Lesson;
+
 public class LessonService {
+	
+	public ArrayList<Lesson> selectMainClass(){
+		Connection conn = getConnection();
+		ArrayList<Lesson> list = new LessonDao().selectMainClass(conn);
+		close(conn);
+		return list;
+	}
 
 }
