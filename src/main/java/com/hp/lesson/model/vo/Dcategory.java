680c9805e88cc91c70f1016f34fcf34e1e305385
you hanbit <115604577+hanbitgithub@users.noreply.github.com>
2023-01-21 21:47:44 +0900

    Update Dcategory.java

--- src/main/java/com/hp/lesson/model/vo/Dcategory.java
+++ src/main/java/com/hp/lesson/model/vo/Dcategory.java
@@ -1,47 +1,37 @@
 package com.hp.lesson.model.vo;
 
-public class DetailCt {
+public class Dcategory {
 	
 	private int ctDno;
 	private String ctNo;
 	private String ctDname;
-	
-	public DetailCt() {}
-
-	public DetailCt(int ctDno, String ctNo, String ctDname) {
+	public Dcategory(int ctDno, String ctNo, String ctDname) {
 		super();
 		this.ctDno = ctDno;
 		this.ctNo = ctNo;
 		this.ctDname = ctDname;
 	}
-
 	public int getCtDno() {
 		return ctDno;
 	}
-
 	public void setCtDno(int ctDno) {
 		this.ctDno = ctDno;
 	}
-
 	public String getCtNo() {
 		return ctNo;
 	}
-
 	public void setCtNo(String ctNo) {
 		this.ctNo = ctNo;
 	}
-
 	public String getCtDname() {
 		return ctDname;
 	}
-
 	public void setCtDname(String ctDname) {
 		this.ctDname = ctDname;
 	}
-
 	@Override
 	public String toString() {
-		return "DetailCt [ctDno=" + ctDno + ", ctNo=" + ctNo + ", ctDname=" + ctDname + "]";
+		return "Dcategory [ctDno=" + ctDno + ", ctNo=" + ctNo + ", ctDname=" + ctDname + "]";
 	}
 	
 	
