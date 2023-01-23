533faa158ff1704782f56adb86f9386a1a3045b7
suuuuj <115604669+suuuuj@users.noreply.github.com>
2023-01-24 02:45:03 +0900

    refundClassList

--- src/main/java/com/hp/member/controller/RefundClassListController.java
+++ src/main/java/com/hp/member/controller/RefundClassListController.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class MyClassViewController
+ * Servlet implementation class RefundClassListController
  */
-@WebServlet("/myClass.tt")
-public class MyClassViewController extends HttpServlet {
+@WebServlet("/refundList.tee")
+public class RefundClassListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MyClassViewController() {
+    public RefundClassListController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,8 +26,7 @@ public class MyClassViewController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("views/member/myClassList.jsp").forward(request, response);
-	
+		request.getRequestDispatcher("views/member/refundClassList.jsp").forward(request, response);
 	}
 
 	/**
---
+++ src/main/webapp/views/member/refundClassList.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+    <%@ include file = "../common/myClassMenubar.jsp" %>
+			
+		<div class="content">
+
+			<!-- 환불한 클래스가 없을때-->
+			<!--
+			<div id="classnull">
+				<img src="<%=contextPath%>/resources/images/myClassNull.jpg" alt="">
+				<p>환불한 클래스가 없습니다.</p>
+				<button type="button" class="btn btn-light" id="findClass" onclick="">클래스 찾아보기</button>
+			</div>-->
+
+</body>
+</html>
