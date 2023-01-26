f8a6c8051a26d8d282162de074bc9322b59e60e9
yunysss <115604544+yunysss@users.noreply.github.com>
2023-01-26 17:13:48 +0900

    관리자 정산관리

--- src/main/java/com/hp/calculate/controller/CalculateManageController.java
+++ src/main/java/com/hp/calculate/controller/CalculateManageController.java
@@ -1,11 +1,16 @@
 package com.hp.calculate.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.hp.calculate.model.service.CalculateService;
+import com.hp.calculate.model.vo.Calculate;
 
 /**
  * Servlet implementation class CalculateManage
@@ -26,6 +31,9 @@ public class CalculateManageController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArrayList<Calculate> list = new CalculateService().selectAllSeasonCalMng();
+		
+		request.setAttribute("list", list);
 		request.getRequestDispatcher("views/calculate/calculateManage.jsp").forward(request, response);
 	}
 
