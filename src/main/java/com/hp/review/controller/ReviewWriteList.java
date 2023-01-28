199a959d63e3d9ee7b8fd67f8523b4970e9feb08
suuuuj <115604669+suuuuj@users.noreply.github.com>
2023-01-28 22:42:37 +0900

    추가수정

--- src/main/java/com/hp/review/controller/ReviewWriteList.java
+++ src/main/java/com/hp/review/controller/ReviewWriteList.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ReviewWriteList
  */
-@WebServlet("/wReviewList.tee")
+@WebServlet("/wReviewList.rev")
 public class ReviewWriteList extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
