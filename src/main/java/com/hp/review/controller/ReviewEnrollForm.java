fd149521feace3288abbb73b39f8d67a28753715
suuuuj <115604669+suuuuj@users.noreply.github.com>
2023-02-02 15:59:39 +0900

    나의 후기 작성리스트

--- src/main/java/com/hp/review/controller/ReviewEnrollForm.java
+++ src/main/java/com/hp/review/controller/ReviewEnrollForm.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class ReviewWriteList
+ * Servlet implementation class ReviewEnrollForm
  */
-@WebServlet("/wReviewList.rev")
-public class ReviewWriteList extends HttpServlet {
+@WebServlet("/enrollForm.rev")
+public class ReviewEnrollForm extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ReviewWriteList() {
+    public ReviewEnrollForm() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,7 +26,8 @@ public class ReviewWriteList extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("views/review/reviewWriteList.jsp").forward(request, response);
+		request.getRequestDispatcher("views/review/reviewEnrollForm.jsp").forward(request, response);
+	
 	
 	}
 
