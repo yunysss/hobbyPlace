9d1f7b17c1111b32b30638938c8aa33fa63bab97
suuuuj <115604669+suuuuj@users.noreply.github.com>
2023-02-05 01:48:13 +0900

    예약관리

--- src/main/java/com/hp/tutor/controller/AjaxReservationListBfBtnController.java
+++ src/main/java/com/hp/tutor/controller/AjaxReservationListBfBtnController.java
@@ -8,8 +8,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.google.gson.Gson;
+import com.hp.member.model.vo.Member;
 import com.hp.register.model.vo.Register;
 import com.hp.tutor.model.service.TutorService;
 
@@ -34,10 +36,15 @@ public class AjaxReservationListBfBtnController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-
-		response.setContentType("application/json; charset=UTF-8");
-		new Gson().toJson(bfList, response.getWriter());
+		HttpSession session = request.getSession();
+		Member loginUser = (Member) session.getAttribute("loginUser");
+	    
+	    int memNo = Integer.parseInt(request.getParameter("loginUser"));
+	   // System.out.println("===memno2=== :  " + memNo2);
+	    
+	    response.setContentType("application/json; charset=UTF-8");
+	    ArrayList<Register> bfList = new TutorService().selectBFClassList(memNo);
+	    new Gson().toJson(bfList, response.getWriter());
 
 	}
 
